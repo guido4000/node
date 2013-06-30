734caaad19706a0e86d969e1e209e40a4c3cc696
guido4000 <guido4000@gmail.com>
2013-06-30 15:38:35 +0200

    initial setup

---
+++ install.sh
@@ -0,0 +1,8 @@
+
+sudo apt-get update
+sudo apt-get install python-software-properties python g++ make
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
+npm install connect
